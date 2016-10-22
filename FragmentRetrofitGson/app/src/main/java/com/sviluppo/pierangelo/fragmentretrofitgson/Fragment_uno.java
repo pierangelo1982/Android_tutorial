f2d56f1c78c94ddd65a180ecfe27f225e8ab0a8a
pierangelo1982 <pierangelo1982@gmail.com>
2016-10-22 10:13:29 +0200

    loop json ok

--- FragmentRetrofitGson/app/src/main/java/com/sviluppo/pierangelo/fragmentretrofitgson/Fragment_uno.java
+++ FragmentRetrofitGson/app/src/main/java/com/sviluppo/pierangelo/fragmentretrofitgson/Fragment_uno.java
@@ -2,27 +2,25 @@ package com.sviluppo.pierangelo.fragmentretrofitgson;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.sviluppo.pierangelo.fragmentretrofitgson.Model.Cluniacensi;
-import com.sviluppo.pierangelo.fragmentretrofitgson.Service.CluniacensiDeserializer;
 import com.sviluppo.pierangelo.fragmentretrofitgson.Service.CluniacensiService;
 
 import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
-import retrofit2.GsonConverterFactory;
 import retrofit2.Response;
-import retrofit2.Retrofit;
 
 
 public class Fragment_uno extends Fragment
 {
+    TextView textTest;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -36,6 +34,16 @@ public class Fragment_uno extends Fragment
             @Override
             public void onResponse(Call<List<Cluniacensi>> call, Response<List<Cluniacensi>> response) {
 
+                if (response.isSuccessful()) {
+                    List<Cluniacensi> clu = response.body();
+                    for (int i = 0; i<clu.size(); i++) {
+                        Cluniacensi c = clu.get(i);
+                        Log.d("test", c.getTitolo());
+                    }
+
+                    textTest = (TextView) getView().findViewById(R.id.textViewTest);
+                    textTest.setText(response.body().toString());
+                }
             }
 
             @Override
