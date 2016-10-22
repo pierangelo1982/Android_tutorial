b471a472fbfc9f7baf6a92c3104d0cec673b71ab
pierangelo1982 <pierangelo1982@gmail.com>
2016-10-22 10:34:07 +0200

    listview

--- FragmentRetrofitGson/app/src/main/java/com/sviluppo/pierangelo/fragmentretrofitgson/Fragment_uno.java
+++ FragmentRetrofitGson/app/src/main/java/com/sviluppo/pierangelo/fragmentretrofitgson/Fragment_uno.java
@@ -6,11 +6,16 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 import com.sviluppo.pierangelo.fragmentretrofitgson.Model.Cluniacensi;
 import com.sviluppo.pierangelo.fragmentretrofitgson.Service.CluniacensiService;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import retrofit2.Call;
@@ -21,6 +26,10 @@ import retrofit2.Response;
 public class Fragment_uno extends Fragment
 {
     TextView textTest;
+    ListView myList;
+
+    ArrayList<HashMap<String, String>> jsonlist = new ArrayList<HashMap<String, String>>();
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -39,10 +48,20 @@ public class Fragment_uno extends Fragment
                     for (int i = 0; i<clu.size(); i++) {
                         Cluniacensi c = clu.get(i);
                         Log.d("test", c.getTitolo());
+                        String title = clu.get(i).getTitolo();
+                        String city = clu.get(i).getCitta();
+
+                        HashMap<String, String> map = new HashMap<String, String>();
+                        map.put("titolo", String.valueOf(title));
+                        map.put("citta", String.valueOf(city));
+
+                        jsonlist.add(map);
                     }
 
-                    textTest = (TextView) getView().findViewById(R.id.textViewTest);
-                    textTest.setText(response.body().toString());
+                    myList = (ListView) getView().findViewById(R.id.myListView);
+                    ListAdapter adapter = new SimpleAdapter(getContext(), jsonlist, R.layout.contenuto_lista,
+                            new String[]{"titolo", "citta"}, new int[]{R.id.labelData, R.id.labelTot});
+                    myList.setAdapter(adapter);
                 }
             }
 
@@ -51,6 +70,7 @@ public class Fragment_uno extends Fragment
 
             }
         });
+
         return view;
     }
 
